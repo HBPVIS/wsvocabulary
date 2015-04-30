ac2ce5c8f1376aba296a5a81e2c885e648e01ad4
Sergio Galindo <sergio.e.galindo@gmail.com>
2015-04-30 12:20:52 +0200

    Subscriber is now static. Code comments added.

--- wsserver/wsserver.cpp
+++ wsserver/wsserver.cpp
@@ -1,10 +1,13 @@
+/* Copyright (c) 2014-2015, Human Brain Project Workshop
+ */
+
 #include <wsvoc/wsvoc.h>
 #include <zeq/zeq.h>
 #include <boost/bind.hpp>
 #include <lunchbox/uri.h>
 
 zeq::Publisher* publisher;
-zeq::Subscriber* subscriber;
+//zeq::Subscriber* subscriber;
 
 using namespace wsvoc;
 
@@ -35,19 +38,41 @@ int main(int /*argc*/, char** argv)
 {
 
 
-
+  /**
+   * Init publisher with URI taken from command line argument as follows:
+   *  - WITH ZeroConf: schema://
+   *  - WITHOUT ZeroConf: schema://ip:port
+   *
+   *  Note: Publisher URI must use its machine IP address.
+   *
+   * */
   publisher = new zeq::Publisher( lunchbox::URI(argv[1] ) );
 
-  subscriber = new zeq::Subscriber( lunchbox::URI(argv[2] ));
+  /**
+   *  When using static publisher/subscriber it's necessary to create URI
+   *  as an object, avoiding to do it on the same line as follows:
+   *  subscriber(lunchbox::URI(uri))
+   *
+   */
+  lunchbox::URI uri (argv[2]);
+  zeq::Subscriber subscriber ( uri );
 
 
-  subscriber->registerHandler( wsvoc::EVENT_VECTOR3D,
+  /**
+   * Init subscriber with URI taken from command line argument as follows:
+   *  - WITH ZeroConf: schema://
+   *  - WITHOUT ZeroConf: schema://ip:port
+   *
+   *  Note: Subscriber URI must use publisher's machine IP address.
+   *
+   * */
+  subscriber.registerHandler( wsvoc::EVENT_VECTOR3D,
                               boost::bind( &ReceiveCallback, _1));
 
   while(true)
   {
 
-    while( subscriber->receive( 200 )){}
+    while( subscriber.receive( 200 )){}
 
 	}
 
